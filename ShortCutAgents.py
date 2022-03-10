1a02a078dded75ab5b9a474ce01bfc8fa1722bee
Koen van der Burg <Koenvanderburg@live.nl>
2022-03-10 15:55:42 +0100

    Q-Learning Policy update
    Initialize made, start on action selection. Fix return statement.

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class QLearningAgent(object):
 
@@ -6,13 +7,19 @@ class QLearningAgent(object):
         self.n_actions = n_actions
         self.n_states = n_states
         self.epsilon = epsilon
-        # TO DO: Add own code
+        action_values = [[0 for i in range (self.n_states)] for j in range (self.n_actions)]        #initialize zero 2-d array [n_states * n_actions]
         pass
         
-    def select_action(self, state):
-        # TO DO: Add own code
-        a = random.choice(range(self.n_actions)) # Replace this with correct action selection
-        return a
+    def select_action(self, states):
+        prob = [[0 for x in range (states)] for j in range(self.n_actions)]                         #create probabilities list.                   
+        for state in range(states):
+            for action in range(self.n_actions):
+                if action == np.argmax(self.mean):
+                    prob[state, action] = (1 - self.epsilon)                                               #assign highest mean of action the highest probability. 
+                else:
+                    prob[state, action] = (self.epsilon/(self.n_actions - 1))                              #assign rest of probability values to rest of the actions.
+        
+        return(np.random.choice(self.n_actions, p = prob))                                          #return a random action based on the probabilities.
         
     def update(self, state, action, reward):
         # TO DO: Add own code
