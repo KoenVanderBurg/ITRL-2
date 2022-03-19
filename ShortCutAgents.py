caf97ed662e444a3cddc5fdbd239be1f3159a783
Koen van der Burg <Koenvanderburg@live.nl>
2022-03-19 16:20:04 +0100

    Q-learning agent finished

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -3,26 +3,29 @@ import numpy as np
 
 class QLearningAgent(object):
 
-    def __init__(self, n_actions, n_states, epsilon):
+    def __init__(self, n_actions, n_states, epsilon, alpha, next_state):
         self.n_actions = n_actions
         self.n_states = n_states
         self.epsilon = epsilon
+        self.alpha = alpha
+        self.next_state = next_state
         action_values = [[0 for i in range (self.n_states)] for j in range (self.n_actions)]        #initialize zero 2-d array [n_states * n_actions]
         pass
         
-    def select_action(self, states):
-        prob = [[0 for x in range (states)] for j in range(self.n_actions)]                         #create probabilities list.                   
-        for state in range(states):
-            for action in range(self.n_actions):
-                if action == np.argmax(self.mean):
-                    prob[state, action] = (1 - self.epsilon)                                               #assign highest mean of action the highest probability. 
-                else:
-                    prob[state, action] = (self.epsilon/(self.n_actions - 1))                              #assign rest of probability values to rest of the actions.
+    def select_action(self, state):
+
+        prob = [[0 for x in range (self.n_states)] for j in range(self.n_actions)]                   #create probabilities list.                   
+        
+        for action in range(self.n_actions):
+            if action == np.argmax(self.action_values[state]):
+                prob[state][action] = (1 - self.epsilon)                                               #assign highest mean of action the highest probability. 
+            else:
+                prob[state][action] = (self.epsilon/(self.n_actions - 1))                              #assign rest of probability values to rest of the actions.
         
         return(np.random.choice(self.n_actions, p = prob))                                          #return a random action based on the probabilities.
         
     def update(self, state, action, reward):
-        # TO DO: Add own code
+        self.action_values[state][action] = self.action_values[state][action] + self.alpha * ( reward + 1 * np.argmax(self.action_values[self.next_state]) - self.action_values[state][action])
         pass
 
 class SARSAAgent(object):
--- ShortCutExperiment.py
+++ ShortCutExperiment.py
@@ -1,1 +1,60 @@
-# Write your experiments in here! You can use the plotting helper functions from the previous assignment if you want.
+import re
+from statistics import mean
+import numpy as np
+from ShortCutEnvironment import ShortcutEnvironment
+from ShortCutAgents import QLearningAgent, SARSAAgent, ExpectedSARSAAgent
+from Helper import LearningCurvePlot, ComparisonPlot, smooth
+
+def run_repetitions_QA(n_actions, n_timesteps, n_repetitions,epsilon):
+    
+
+    
+    all_rewards = np.zeros((n_repetitions,n_timesteps))                    #2d-array to store all the rewards in of the n_repititions * n_timesteps.
+
+    for repetition in range(n_repetitions):                                #for each repetition:
+
+        env = BanditEnvironment(n_actions=n_actions)                            # initialize environment    
+        pi = EgreedyPolicy(n_actions=n_actions)                                 # initialize policy
+        rewards =np.zeros(n_timesteps)                                          # initialize rewards
+        for timestep in range(n_timesteps):                                #for each repetition and timestep:
+            a = pi.select_action(epsilon)                                        #select action
+            r = env.act(a)                                                       #sample reward
+            pi.update(a,r)                                                       #update policy
+            rewards[timestep] = r                                                #store reward into rewards
+
+        all_rewards[repetition] = rewards
+
+    return np.average(all_rewards, 0)                                      #return the average over all the rewards
+
+def experiment():
+
+    # Assignment 1: e-greedy
+    plot = LearningCurvePlot('Learning Curves Epsilon Greedy')
+    epsilons = [ 0.01,0.05,0.1,0.25]
+    best_return_greedy = 0.0
+    best_run_greedy = [0 for x in range(n_timesteps)]                                   
+    average_returns_greedy = []
+    for epsilon in epsilons:
+        rewards = run_repetitions_eg(n_actions, n_timesteps, n_repetitions, epsilon)
+        average_reward = np.average(rewards)                                            #getting the average rewards over all the rewards.
+        average_returns_greedy.append(average_reward)
+        if average_reward > best_return_greedy:                                         #if the average_reward is better than the so far best, the average_reward becomes the new best.
+            best_return_greedy = average_reward
+            best_run_greedy = rewards                                                   #the accompanying best run gets stored as well.
+
+
+        plot.add_curve(rewards)
+        plot.add_curve(smooth(rewards,smoothing_window), label = f'{epsilon = }')
+  
+    plot.save("EG_4.png")
+
+if __name__ == '__main__':
+    # experiment settings
+    n_episodes = 1000
+    n_rep = 100
+    alpha = 0.1
+    epsilon = 0.1
+    smoothing_window = 31
+
+    experiment(n_episodes = n_episodes,n_rep = n_rep, alpha=alpha, epsilon = epsilon, smoothing_window=smoothing_window)
+
