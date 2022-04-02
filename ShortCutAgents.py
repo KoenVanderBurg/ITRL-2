df1849c42ffb2e4f14b6b6aa0161b16fabe838f5
Koen van der Burg <Koenvanderburg@live.nl>
2022-04-02 19:05:23 +0200

    Experiment 1 run

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -3,30 +3,31 @@ import numpy as np
 
 class QLearningAgent(object):
 
-    def __init__(self, n_actions, n_states, epsilon, alpha, next_state):
+    def __init__(self, n_actions, n_states, epsilon, alpha):
         self.n_actions = n_actions
         self.n_states = n_states
         self.epsilon = epsilon
         self.alpha = alpha
-        self.next_state = next_state
-        action_values = [[0 for i in range (self.n_states)] for j in range (self.n_actions)]        #initialize zero 2-d array [n_states * n_actions]
+        action_values = [[0 for i in range (self.n_actions)] for j in range (self.n_states)]        #initialize zero 2-d array [n_states * n_actions]
         self.action_values = action_values
         pass
         
     def select_action(self, state):
 
-        prob = [[0 for x in range (self.n_states)] for j in range(self.n_actions)]                   #create probabilities list.                   
-        
+        prob = [[0 for x in range (self.n_actions)] for j in range(self.n_states)]                   #create probabilities 2-d array  
+
         for action in range(self.n_actions):
+            print(action)
             if action == np.argmax(self.action_values[state]):
                 prob[state][action] = (1 - self.epsilon)                                               #assign highest mean of action the highest probability. 
             else:
                 prob[state][action] = (self.epsilon/(self.n_actions - 1))                              #assign rest of probability values to rest of the actions.
         
-        return(np.random.choice(self.n_actions, p = prob))                                          #return a random action based on the probabilities.
+        return(np.random.choice(self.n_actions, p = prob[state]))                                          #return a random action based on the probabilities.
         
     def update(self, state, action, reward):
-        self.action_values[state][action] = self.action_values[state][action] + self.alpha * ( reward + 1 * np.argmax(self.action_values[self.next_state]) - self.action_values[state][action])
+        next_state = (state + 1)
+        self.action_values[state][action] = self.action_values[state][action] + self.alpha * ( reward + 1 * np.argmax(self.action_values[next_state]) - self.action_values[state][action])
         pass
 
 class SARSAAgent(object):
--- ShortCutExperiment.py
+++ ShortCutExperiment.py
@@ -5,38 +5,36 @@ from ShortCutEnvironment import ShortcutEnvironment
 from ShortCutAgents import QLearningAgent, SARSAAgent, ExpectedSARSAAgent
 from Helper import LearningCurvePlot, ComparisonPlot, smooth
 
-def run_repetitions_QA (n_actions, n_episodes, alpha , epsilon, next_state, n_states, n_rep):
-    
+def run_repetitions_QA (n_actions, n_episodes, alpha , epsilon, n_rep):
     all_rewards = np.zeros((n_episodes,n_actions))                    #2d-array to store all the rewards in of the n_repetitions * n_episodes.
 
     for rep in range(n_rep):                                                     #for each repetition:
-        env = ShortcutEnvironment()                                              # initialize environment    
+        env = ShortcutEnvironment()                                             # initialize environment  
+        n_states = env.state_size()  
         pi = QLearningAgent(n_actions = n_actions, n_states = n_states,
-                             alpha = alpha, epsilon = epsilon, alpha = alpha,
-                             next_state = next_state)                            # initialize policy
+                             alpha = alpha, epsilon = epsilon)                            # initialize policy
         rewards =np.zeros(n_episodes)                                            # initialize rewards
         for episode in range(n_episodes):                                        #for each repetition and timestep
+            start = env.reset()
             while env.isdone == False:
-
                 a = pi.select_action(env.state())                                    #select action
                 r = env.step(a)                                                      #fetch reward
-                pi.update(a,r)                                                       #update policy
+                pi.update(env.state(),a,r)                                                       #update policy
                 rewards[episode] = r                                                #store reward into rewards
 
         all_rewards[episode] = rewards
-
     return np.average(all_rewards, 0)                                      #return the average over all the rewards
 
-def experiment():
+def experiment(n_actions, n_episodes, n_rep, alpha, epsilon, smoothing_window):
 
     # Assignment 1: e-greedy
     plot = LearningCurvePlot('Learning Curves Epsilon Greedy')
-    epsilons = [ 0.01,0.05,0.1,0.25]
+    epsilons = [ 0.01]
     best_return_greedy = 0.0
-    best_run_greedy = [0 for x in range(n_timesteps)]                                   
+    best_run_greedy = [0 for x in range(n_rep)]                                   
     average_returns_greedy = []
     for epsilon in epsilons:
-        rewards = run_repetitions_eg(n_actions, n_timesteps, n_repetitions, epsilon)
+        rewards = run_repetitions_QA(n_actions, n_episodes, alpha, epsilon, n_rep)
         average_reward = np.average(rewards)                                            #getting the average rewards over all the rewards.
         average_returns_greedy.append(average_reward)
         if average_reward > best_return_greedy:                                         #if the average_reward is better than the so far best, the average_reward becomes the new best.
@@ -47,7 +45,7 @@ def experiment():
         plot.add_curve(rewards)
         plot.add_curve(smooth(rewards,smoothing_window), label = f'{epsilon = }')
   
-    plot.save("EG_4.png")
+    plot.save("QA_1.png")
 
 if __name__ == '__main__':
     # experiment settings
